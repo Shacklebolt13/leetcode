ce4bac96d453081edc63ccdef5a4a0e153e13d3d
Gagandeep Singh Bhambrah <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-08-02 05:22:30 +0000

    Sync LeetCode submission - Kth Smallest Element in a Sorted Matrix (python3)

---
+++ problems/kth_smallest_element_in_a_sorted_matrix/solution.py
@@ -0,0 +1,8 @@
+from itertools import chain
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        return  sorted(chain.from_iterable(matrix))[k-1]
+        
+        
+        
